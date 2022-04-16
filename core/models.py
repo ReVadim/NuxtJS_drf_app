327c89a7c07976776c5687ab56b499d464611b67
ReVadim <revani@yandex.ru>
2022-04-16 17:36:32 +0300

    start drf_project

---
+++ core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    """ Model Post """
+    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = RichTextUploadingField()
+    content = RichTextUploadingField()
+    image = models.ImageField()
+    created_at = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
